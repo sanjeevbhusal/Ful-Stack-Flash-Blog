81ad6a6bc4ad94e68f6bc9465ca0cc500b3a699b
Sanjeev <bhusalsanjeev23@gmail.com>
2022-05-05 01:06:56 +0545

    Added templatesm static directory. Added Bootsrap and custom CSS. Updated HTML files.

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+posts = [
+    {
+        "author" : "Snajeev",
+        "title" : "How to be Better Developer",
+        "content" : "Learn EveryDay",
+        "date_posted": "2022/05/05",
+    }
+]
+
 @app.route("/")
 def home_page():
-    return "Hello World"
- 
+    return render_template("app.html", title = "Home Page", posts = posts)
+
+@app.route("/about")
+def about_page():
+    return render_template("about.html", title = "About", body = "This is an about Page")
  
 if __name__ == "__main__":
     app.run(debug = True)
